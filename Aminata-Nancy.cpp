b69a76598854ed1b9977a790a90363c929a34b25
mameami26 <mameami26@gmail.com>
2024-09-28 15:26:38 -0400

    put the instruction of the assessment

--- Aminata-Nancy.cpp
+++ Aminata-Nancy.cpp
@@ -1,1 +1,22 @@
+/*
+Instructions
+Write a program in C++ to convert a decimal number (base 10) to binary (Base 2 )using recursion.
 
+Have your program reprompt the user until the user types -1
+
+
+----------------------------Sample run -----------------------------------
+
+
+Enter a decimal number : 66
+The Binary value of decimal no. 66 is : 1000010
+
+Enter a decimal number : 16
+The Binary value of decimal no. 16 is : 1000
+
+Enter a decimal number : 57
+The Binary value of decimal no. 57 is : 111001
+
+Enter a decimal number : -1
+
+The program stope.*/
