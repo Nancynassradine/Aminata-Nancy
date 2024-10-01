193fab004c83ada5d30239d91504f6d56e755fcf
mameami26 <mameami26@gmail.com>
2024-09-30 21:37:46 -0400

    full code

--- Aminata-Nancy.cpp
+++ Aminata-Nancy.cpp
@@ -1,25 +1,37 @@
-/*
-Instructions
-Write a program in C++ to convert a decimal number (base 10) to binary (Base 2 )using recursion.
-
-Have your program reprompt the user until the user types -1
-
-
-----------------------------Sample run -----------------------------------
-
-
-Enter a decimal number : 66
-The Binary value of decimal no. 66 is : 1000010
-
-Enter a decimal number : 16
-The Binary value of decimal no. 16 is : 1000
-
-Enter a decimal number : 57
-The Binary value of decimal no. 57 is : 111001
-
-Enter a decimal number : -1
-
-The program stope.*/
+#include <iostream>
+using namespace std;
 
 #include <iostream>
 using namespace std;
+
+void convertToBinary(int num) {
+    if (num == 0) {
+        return;
+    }
+    convertToBinary(num / 2);
+    cout << (num % 2)<<endl;
+}
+
+int main() {
+    int input;
+
+    while (true) {
+        cout << "Enter a decimal number : ";
+        cin >> input;
+
+        if (input == -1) {
+            cout << "The program stopped." << endl;
+            break;
+        }
+
+        if (input == 0) {
+            cout << "The Binary value of decimal no. " << input << " is : 0" << endl;
+        } else {
+            cout << "The Binary value of decimal no. " << input << " is : ";
+            convertToBinary(input);
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
